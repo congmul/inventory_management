2b862162679e7d6ebb65f6ae4cb47cde2c171ce0
Jehyun <congmul@hotmail.com>
2020-12-22 14:30:43 -0800

    Modifies upload CSV

--- public/js/uploadcsv.js
+++ public/js/uploadcsv.js
@@ -1,11 +1,12 @@
 
 $(document).ready(function () {
+    let contents = [];
 
-    $("#uploadcsvbtn").on("click", (e) => {
+    $("#checking-csv-btn").on("click", (e) => {
         e.preventDefault();
         let rdr = new FileReader();
         let csvData;
-        let contents = [];
+        
         rdr.onload = (e) => {
             console.log(e.target.result);
             csvData = e.target.result.split(/\r\n|\n/);
@@ -18,17 +19,28 @@ $(document).ready(function () {
                         "group_code" :tempArr[2],
                         "dealer_price" :tempArr[3],
                     });
+                let trEl = $("<tr>");
+                for(let j = 0; j < tempArr.length; j++){
+                    trEl.append($("<td>").text(tempArr[j])); 
+                }    
+                   
+                $("#table-packages-Uploaded").append(trEl);
             }
-            console.log(contents);
-
-            $.post("/api/packages",{contents:contents}).then((res) =>{
-
-            })
-            
+            $("#documentTemplate").css("display", "none");
+            $("#packages-Uploaded").css("display", "block");
         }
         rdr.readAsText($("#csvFileUpload")[0].files[0]);
-    })
+    });
 
+    $("#uploadcsvbtn").on("click", (e) =>{
+        e.preventDefault();
+        console.log(contents);
+        $.post("/api/packages",{contents:contents}).then((res) =>{
+            alert("The file is uploaded");
+            location.reload();
+        });
+    });
+    
 
 
 });
