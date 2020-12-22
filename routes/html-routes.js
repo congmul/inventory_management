651ee3598096e3650da845b29c85bb51f8c563e4
Jehyun <congmul@hotmail.com>
2020-12-21 16:11:43 -0800

    Modifies login and logout page with html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,23 +10,12 @@ module.exports = function(app) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.render("index");
+    }else{
+      res.sendFile(path.join(__dirname, "../public/login.html"));
     }
-    res.render("login");
-    // res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
-  app.get("/login", function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+  app.get("/signup", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
-
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
-  });
-
 };
