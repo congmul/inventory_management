522e6556e53d63df7dc46bf8dbf69aa2e5f9072e
Jehyun <congmul@hotmail.com>
2020-12-23 23:55:26 -0800

    Add gross margin net on modal

--- public/js/viewAll.js
+++ public/js/viewAll.js
@@ -10,40 +10,90 @@ $(document).ready(() => {
             type: "GET"
         }).then(res => {
             console.log(res);
-            let query = `  <div id="packageWithCymbal" class="modal">
-            <div class="modal-content">
-            <table class="highlight centered">
-            <thead style="font-weight: bold;">
+            let ebayGross = 0;
+            let websiteGross = 0;
+            let ebayNet = 0;
+            let websiteNet = 0;
+            
+            let cymbalsQuery = ``;
+            for(let i = 0; i < res.length; i++){
+                ebayGross += res[i].ebay_price;
+                websiteGross += res[i].website_price;
+                cymbalsQuery += `
                 <tr>
-                    <th>Package Code</th>
-                    <th>Category</th>
-                    <th>Description</th>
-                    <th>Dealer Price</th>
-                    <th>Net</th>
-                    <th>eBay Margin</th>
-                    <th>website Margin</th>
-                </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>${res[0].code_pack}</td>
-                <td>${res[0].category}</td>
-                <td>${res[0].description}</td>
-                <td>${res[0].dealer_price}</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-        </tbody>
-    </table>
-            </div>
-            <div class="modal-footer">
-              <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
-            </div>
-          </div>`
+                    <td>${res[i].code}</td>
+                    <td>${res[i].description}</td>
+                    <td>${res[i].category02}</td>
+                    <td>${res[i].size} inch</td>
+                    <td>$ ${res[i].ebay_price}</td>
+                    <td>$ ${res[i].website_price}</td>
+                </tr>`
+            }
+            ebayNet = ebayGross * 0.88;
+            websiteNet = websiteGross * 0.96;
+
+            let ebayMargin =  (ebayNet - res[0].dealer_price) / res[0].dealer_price  * 100;
+            let websiteMargin = (websiteNet - res[0].dealer_price) / res[0].dealer_price  * 100;
+        
+
+            let query = `  
+<div id="packageWithCymbal" class="modal">
+    <div class="modal-content">
+        <h5>${res[0].category} ${res[0].description_pack} |  ${res[0].code_pack}  |  $${res[0].dealer_price}</h5>
+            <table class="highlight centered" style="margin-top:25px;">
+                <thead style="font-weight: bold;">
+                    <tr>
+                        <th>Code</th>
+                        <th>Description</th>
+                        <th>Type</th>
+                        <th>Size</th>
+                        <th>eBay Price</th>
+                        <th>Website Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                ${cymbalsQuery}
+                </tbody>
+            </table>
+        <!--Gross Fee Margin-->
+        <div style="margin-top:25px;">
+        <table class="highlight centered">
+<thead style="font-weight: bold;">
+<tr>
+                        <th>Platform</th>
+                        <th>Gross</th>
+                        <th>Fee</th>
+                        <th>Net</th>
+                        <th>Margin</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td style="font-weight: bold;">ebay : </td>
+                        <td>$ ${ebayGross}</td>
+                        <td>12%</td>
+                        <td>$ ${ebayNet.toFixed(2)}</td>
+                        <td>${ebayMargin.toFixed(2)}%</td>
+                    </tr>
+                    <tr>
+                        <td style="font-weight: bold;">website : </td>
+                        <td>$ ${websiteGross}</td>
+                        <td>4%</td>
+                        <td>$ ${websiteNet.toFixed(2)}</td>
+                        <td>${websiteMargin.toFixed(2)}%</td>
+                    </tr>
+                </tbody>
+        </table>
+        </div>
+    </div>
+    <div class="modal-footer">
+        <a href="#!" class="modal-close waves-effect waves-green btn-flat">close</a>
+    </div>
+</div>`
           
           displayPackwithCymbals.html(query);
+
+          // Implement modal and Open it manually
           $("#packageWithCymbal").modal();
           $("#packageWithCymbal").modal('open');
         })
